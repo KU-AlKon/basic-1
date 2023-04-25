4073f511691e541826481b138b2b563f64b638ca
Jinwon Kim <104982917+jinwon0988@users.noreply.github.com>
2023-04-25 22:24:38 +0900

    3040번

---
+++ 3040.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int dwarf[9];
+
+    int sum=0;
+    for(int i=0; i<9; i++) {
+        cin >> dwarf[i];
+        sum += dwarf[i];
+    }
+
+    sum -= 100;
+
+    for(int i=0; i<8; i++) {
+        for(int j=i+1; j<9; j++) {
+            if((dwarf[i] + dwarf[j]) == sum) {
+                dwarf[i] = dwarf[j] = 0;
+                break;
+            }
+        }
+        if(!dwarf[i])
+            break;
+    }
+
+    for(int i=0; i<9; i++){
+        if(dwarf[i])
+            cout << dwarf[i] << '\n';
+    }
+}
