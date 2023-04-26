fe0b9b594cd03fe5e296ac6e3558925c507f1d17
0dimen <76515856+0dimen@users.noreply.github.com>
2023-04-27 01:38:10 +0900

    김채원_1주차

---
+++ Week1/1193_dimens.cpp
@@ -0,0 +1,20 @@
+// 분수 찾기 : https://www.acmicpc.net/problem/1193
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int input;
+    cin >> input;
+
+    int i = 1;
+    for(; input>i; i++)
+        input -= i;
+
+    if(i%2 == 0) //짝수면 내려감.
+        cout << input << "/" << i+1-input;
+
+    else // 홀수면 올라감.
+        cout << i+1-input << "/" << input;
+    
+}
---
+++ Week1/2444_dimens.cpp
@@ -0,0 +1,29 @@
+// 별 찍기 : https://www.acmicpc.net/problem/2444
+
+#include <iostream>
+using namespace std;
+
+void printStar(int i, int spaceNum){
+    string str;
+    for(int j = 0; j < spaceNum ; j++)
+        str += " ";
+
+    str += "*";
+    for(int j = 0; j < i; j++)
+        str += "**";
+    
+    cout << str << "\n";
+}
+
+int main(){
+    int N;
+    cin >> N;
+
+    for(int i = 0; i < N; i++)
+        printStar(i, N-i-1);
+    
+    N--;
+
+    for (int i = 1; i < N +1; i++)
+        printStar(N-i, i);
+}
